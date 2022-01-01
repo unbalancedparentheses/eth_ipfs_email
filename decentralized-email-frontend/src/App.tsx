76312a713666a7bfe94027ee382e41be39f89668
MauroFab <maurotoscano2@gmail.com>
2022-01-01 20:17:26 -0300

    Add state to test

--- decentralized-email-frontend/src/App.tsx
+++ decentralized-email-frontend/src/App.tsx
@@ -7,16 +7,29 @@ import { ethers } from "ethers";
 function App() {
   const provider = new ethers.providers.Web3Provider((window as any).ethereum);
 
+  const [accounts, setAccounts] = useState<string[]>([""]);
+
+
   async function getAccounts(provider: any) {
     const val = await provider.send("eth_requestAccounts", []);
-    console.log(val);
+    setAccounts(val)
+    console.log(val)
+    console.log(accounts)
   }
 
   return (
-    <div >
-      <Button type="primary" onClick={async () => { await getAccounts(provider); }}>
-        Accounts
-      </Button>
+    <div>
+      <div>      
+        <b>
+        Hola
+        </b>
+        <b>
+          Tus cuentas conectadas son: {accounts}
+        </b>
+      </div>
+    <Button type="primary" onClick={async () => { await getAccounts(provider); }}>
+      Refresh
+    </Button>
     </div>
   );
 }
