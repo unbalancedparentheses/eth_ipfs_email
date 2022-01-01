3af61127dcedfbc808f27cee2998fe74e90010cb
franfiuba <franfiuba@gmail.com>
2022-01-01 19:59:32 -0300

    added metamask connection

--- decentralized-email-frontend/src/App.tsx
+++ decentralized-email-frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'antd';
 import './App.css';
+import { ethers } from "ethers";
+
 
 function App() {
+  const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+
+  async function getAccounts(provider: any) {
+    const val = await provider.send("eth_requestAccounts", []);
+    console.log(val);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Button type="primary" onClick={async () => { await getAccounts(provider); }}>
+        Accounts
+      </Button>
     </div>
   );
 }
