00c2f651f6eedad52e15a4a4d31447a0ba28eaac
MauroFab <maurotoscano2@gmail.com>
2022-01-01 23:36:13 -0300

    Add foundry

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+init:
+	cargo install --git https://github.com/gakonst/foundry --bin forge --locked
 run:
 	(cd decentralized-email-frontend && npm install && npm start) 
--- shell.nix
+++ shell.nix
@@ -5,6 +5,8 @@ with pkgs;
 let
   inherit (lib) optional optionals;
   nodejs = nodejs-16_x;
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
 in
 
 mkShell {
@@ -12,11 +14,16 @@ mkShell {
     nodejs
     git
     nodePackages.ganache-cli
+    libiconv
+    nixpkgs.latest.rustChannels.nightly.rust
   ]
   ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
   ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
     # For file_system on macOS.
     CoreFoundation
     CoreServices
+    AppKit
   ]);
+
+  RUSTFLAGS = "-C link-arg=-undefined -C link-arg=dynamic_lookup";
 }
