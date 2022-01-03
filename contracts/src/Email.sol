a42527deeabe5ce0a96f8acd736bda75b449dd81
Tomás Arjovsky <t.arjovsky@gmail.com>
2022-01-03 00:33:44 -0300

    [WIP] Email contract

---
+++ contracts/src/Email.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.11;
+
+contract EmailContract {
+    struct Message {
+        address from;
+        address to;
+        uint32 ipfs_hash;
+    }
+
+    mapping(address => Message[]) private messages_to;
+
+    function send(
+        address from,
+        address to,
+        uint32 ipfs_hash
+    ) external {
+        Message[] storage messages = messages_to[to];
+        messages.push(Message(from, to, ipfs_hash));
+    }
+
+    function list_messages(address to)
+        external
+        view
+        returns (Message[] memory)
+    {
+        return messages_to[to];
+    }
+}
