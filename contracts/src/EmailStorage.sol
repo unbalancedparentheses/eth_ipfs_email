cc4f4c0ac4eb049e9655f6e5ce92474e2f2b09f4
MauroFab <maurotoscano2@gmail.com>
2022-01-05 11:45:22 -0300

    From addr doesn't need to be asked

--- contracts/src/EmailStorage.sol
+++ contracts/src/EmailStorage.sol
@@ -11,12 +11,11 @@ contract EmailStorage {
     mapping(address => Message[]) private messages_to;
 
     function send(
-        address from,
         address to,
         uint32 ipfs_hash
     ) external {
         Message[] storage messages = messages_to[to];
-        messages.push(Message(from, to, ipfs_hash));
+        messages.push(Message(msg.sender, to, ipfs_hash));
     }
 
     function list_messages(address to)
---
+++ contracts/src/test/EmailStorage.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.11;
+
+import "ds-test/test.sol";
+
+contract EmailStorageTest is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+
+    function testExample2() public {
+        assertTrue(false);
+    }
+}
